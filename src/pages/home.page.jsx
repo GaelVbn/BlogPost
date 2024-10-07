052e2f09e418c818c5029ee9ead1ca8d304dfdf3
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-07 18:24:18 +0200

    rendu card

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import { useEffect, useState } from "react";
+import Loader from "../components/loader.component";
+import BlogPostCard from "../components/blog-post.component";
 
 const HomePage = () => {
+  let [blogs, setBlog] = useState(null);
+  const fetchLatestBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+      .then(({ data }) => {
+        setBlog(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, []);
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10">
@@ -11,7 +30,25 @@ const HomePage = () => {
             routes={["home", "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
-            <h1>Latest blog here</h1>
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
+                    >
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
+              )}
+            </>
 
             <h1>Trending blogs here</h1>
           </InPageNavigation>
