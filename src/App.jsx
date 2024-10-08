c9b567d1e65cae497ca0ddb4a4826dd91323d461
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-08 16:55:19 +0200

    User Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.page";
 import HomePage from "./pages/home.page";
 import SearchPage from "./pages/search.page";
+import ProfilePage from "./pages/profile.page";
 
 export const UserContext = createContext({});
 
@@ -31,6 +32,7 @@ const App = () => {
           <Route path="signin" element={<UserAuthForm type="sign-in" />} />
           <Route path="signup" element={<UserAuthForm type="sign-up" />} />
           <Route path="search/:query" element={<SearchPage />} />
+          <Route path="user/:id" element={<ProfilePage />} />
         </Route>
       </Routes>
     </UserContext.Provider>
--- src/pages/search.page.jsx
+++ src/pages/search.page.jsx
@@ -8,11 +8,13 @@ import NoDataMEssage from "../components/nodata.component";
 import LoadMoreDataBtn from "../components/load-more.component";
 import { filterPaginationData } from "../common/filter-pagination-data";
 import axios from "axios";
+import UserCard from "../components/usercard.component";
 
 const SearchPage = () => {
   let { query } = useParams();
 
   let [blogs, setBlog] = useState(null);
+  let [users, setUsers] = useState(null);
 
   const searchBlogs = ({ page = 1, create_new_arr = false }) => {
     axios
@@ -37,13 +39,46 @@ const SearchPage = () => {
       });
   };
 
+  const fetchUsers = () => {
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/search-users", { query })
+      .then(({ data: { users } }) => {
+        setUsers(users);
+      });
+  };
+
   useEffect(() => {
     resetState();
     searchBlogs({ page: 1, create_new_arr: true });
+    fetchUsers();
   }, [query]);
 
   const resetState = () => {
     setBlog(null);
+    setUsers(null);
+  };
+
+  const UserCardWrapper = () => {
+    return (
+      <>
+        {users == null ? (
+          <Loader />
+        ) : users.length ? (
+          users.map((user, i) => {
+            return (
+              <AnimationWrapper
+                key={i}
+                transition={{ duration: 1, delay: i * 0.08 }}
+              >
+                <UserCard user={user} />
+              </AnimationWrapper>
+            );
+          })
+        ) : (
+          <NoDataMEssage message="no user found" />
+        )}
+      </>
+    );
   };
 
   return (
@@ -75,7 +110,16 @@ const SearchPage = () => {
             )}
             <LoadMoreDataBtn state={blogs} fetchDataFun={searchBlogs} />
           </>
+
+          <UserCardWrapper />
         </InPageNavigation>
+      </div>
+
+      <div className="min-w-[40%] lg:min-w-[350px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+        <h1 className="font-medium text-xl mb-8">
+          User related to search <i className="fi fi-rr-user mt-1"></i>
+        </h1>
+        <UserCardWrapper />
       </div>
     </section>
   );
