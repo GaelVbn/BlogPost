83ad7598e4eeadcbfab2d28c35b1cd476798eb47
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-09 23:13:48 +0200

    deleting comment

--- src/components/comment-card.component.jsx
+++ src/components/comment-card.component.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 const CommentCard = ({ index, leftVal, commentData }) => {
   let {
     commented_by: {
-      personal_info: { profile_img, fullname, username },
+      personal_info: { profile_img, fullname, username: commented_by_username },
     },
     commentedAt,
     comment,
@@ -21,18 +21,39 @@ const CommentCard = ({ index, leftVal, commentData }) => {
     blog,
     blog: {
       comments,
+      activity,
+      activity: { total_parent_comments },
       comments: { results: commentsArr },
+      author: {
+        personal_info: { username: blog_author },
+      },
     },
     setBlog,
+    setTotalParentCommentsLoaded,
   } = useContext(BlogContext);
 
   let {
-    userAuth: { access_token },
+    userAuth: { access_token, username },
   } = useContext(UserContext);
 
   const [isReplying, setReplying] = useState(false);
 
-  const removeCommentsCards = (startingPoint) => {
+  const getParentIndex = () => {
+    let startingPoint = index - 1;
+
+    try {
+      while (
+        commentsArr[startingPoint].childrenLevel >= commentData.childrenLevel
+      ) {
+        startingPoint--;
+      }
+    } catch {
+      startingPoint = undefined;
+    }
+    return startingPoint;
+  };
+
+  const removeCommentsCards = (startingPoint, isDelete = false) => {
     if (commentsArr[startingPoint]) {
       while (
         commentsArr[startingPoint].childrenLevel > commentData.childrenLevel
@@ -44,7 +65,35 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         }
       }
     }
-    setBlog({ ...blog, comments: { results: commentsArr } });
+    if (isDelete) {
+      let parentIndex = getParentIndex();
+
+      if (parentIndex != undefined) {
+        commentsArr[parentIndex].children = commentsArr[
+          parentIndex
+        ].children.filter((child) => child != _id);
+
+        if (!commentsArr[parentIndex].children.length) {
+          commentsArr[parentIndex].isReplyLoaded = false;
+        }
+      }
+
+      commentsArr.splice(index, 1);
+    }
+
+    if (commentData.childrenLevel == 0 && isDelete) {
+      setTotalParentCommentsLoaded((preVal) => preVal - 1);
+    }
+    setBlog({
+      ...blog,
+      comments: { results: commentsArr },
+      activity: {
+        ...activity,
+        total_parent_comments:
+          total_parent_comments -
+          (commentData.childrenLevel == 0 && isDelete ? 1 : 0),
+      },
+    });
   };
 
   const loadReplies = ({ skip = 0 }) => {
@@ -73,6 +122,28 @@ const CommentCard = ({ index, leftVal, commentData }) => {
     }
   };
 
+  const deleteComment = (e) => {
+    e.target.setAttribute("disable", true);
+
+    axios
+      .post(
+        import.meta.env.VITE_SERVER_DOMAIN + "/delete-comment",
+        { _id },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      )
+      .then(() => {
+        e.target.removeAttribute("disable");
+        removeCommentsCards(index + 1, true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const hideReplies = () => {
     commentData.isReplyLoaded = false;
 
@@ -94,7 +165,7 @@ const CommentCard = ({ index, leftVal, commentData }) => {
           <img src={profile_img} alt="image" className="w-6 h-6 rounded-full" />
 
           <p className="line-clamp-1">
-            {fullname} @{username}
+            {fullname} @{commented_by_username}
           </p>
           <p className="min-w-fit">{getDay(commentedAt)}</p>
         </div>
@@ -120,6 +191,16 @@ const CommentCard = ({ index, leftVal, commentData }) => {
           <button className="underline" onClick={handleReplyClick}>
             Reply
           </button>
+          {username == commented_by_username || username == blog_author ? (
+            <button
+              className="p-2 px-3 rounded-md border border-grey ml-auto hover:bg-red/30 hover:text-red flex items-center"
+              onClick={deleteComment}
+            >
+              <i className="fi fi-rr-trash pointer-events-none"></i>
+            </button>
+          ) : (
+            ""
+          )}
         </div>
 
         {isReplying ? (
